d9f16ea0e1516958ce76ccb2c95fba78872c962b
Pranav K <prkrishn@hotmail.com>
2018-02-28 08:49:14 -0800

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLib;
+using Microsoft.Extensions.DependencyModel;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classLibAssembly = typeof(Class1).Assembly;
+            var deps = DependencyContext.Load(classLibAssembly);
+
+            foreach (var library in deps.CompileLibraries)
+            {
+                Console.WriteLine(string.Join(" ", library.ResolveReferencePaths()));
+            }
+        }
+    }
+}
